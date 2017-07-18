ca138025e9edc1e5ef1d5cfc0e705cf839129f0e
EOSullivanBerlin <esully666@gmail.com>
2017-07-18 14:38:23 +0200

    form

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 
-get '/' do
+get '/random-cat' do
   @name =["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
+
+get '/named-cat' do
+p params
+  @name = params[:name]
+  erb(:index)
+end
+
 
 get '/2' do
   'Frog'
